4f441189887011ab64bcf5b5924478ba70be71ea
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-05-11 03:02:27 +0300

    finish services start portfolio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// section modal
+
+const serviceModal = document.querySelectorAll(".service-modal")
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn")
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn")
+
+const modal = function(modalClick){
+    serviceModal[modalClick].classList.add("active")
+}
+
+learnMoreBtn.forEach((button, i) => {
+    button.addEventListener("click", function(){
+        modal(i)
+    })
+})
+
+modalCloseBtn.forEach(button => {
+    button.addEventListener("click", () =>{
+        serviceModal.forEach(modal => {
+            modal.classList.remove("active")
+        })
+    })
+})
