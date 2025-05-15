2e38f0643a22a343d147931cf45103ec81012e36
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-05-15 21:56:01 +0300

    finish js_headerscroll start animation_header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
-// SETVICES section modal
+// =============== HEADER effect scroll ===============
+const header = document.querySelector(".header")
+window.addEventListener("scroll", function(){
+    this.window.scrollY > 10 ? header.classList.add("sticky") : header.classList.remove("sticky")
+}) 
 
+            //addEventListener -  Додає обробник події до об'єкта.
+            // querySelector - для пошуку першої кнопки на веб-сторінці.
+
+
+
+// =============== NAV-ITEM effect color ===============
+window.addEventListener("scroll", function(){
+    const section = this.document.querySelectorAll("section")
+    const scrollY = window.scrollY;
+
+    section.forEach(function(current){
+        let sectionHight = current.offsetHeight
+        let sectionTop = current.offsetTop - 50
+        let sectionId = current.getAttribute("id")
+        let navItem = document.querySelector(`.nav-item a[href*="${sectionId}"]`)
+
+        if(navItem){
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHight){
+            navItem.classList.add("active")
+            } else{
+            navItem.classList.remove("active")
+            }  
+        }
+    })
+})
+
+
+
+// =============== SCROLL TO TOP BUTTON ===============
+const scrollToTop = document.querySelector(".scrollToTop")
+window.addEventListener("scroll", function(){
+    scrollToTop.classList.toggle("active", this.window.scrollY > 500)
+})
+
+scrollToTop.addEventListener("click", function(){
+    document.body.scrollToTop = 0
+    document.documentElement.scrollTop = 0
+})
+
+
+
+// =============== SETVICES section modal ===============
 const serviceModal = document.querySelectorAll(".service-modal")
 const learnMoreBtn = document.querySelectorAll(".learn-more-btn")
 const modalCloseBtn = document.querySelectorAll(".modal-close-btn")
@@ -22,8 +68,9 @@ modalCloseBtn.forEach(button => {
     })
 })
 
-// PORTFOLIO section modal
 
+
+// =============== PORTFOLIO section modal ===============
 const portfolioModals = document.querySelectorAll(".portfolio-modal")
 const imgCard = document.querySelectorAll(".img-card")
 const portfolioCloseBtn = document.querySelectorAll(".portfolio-close-btn")
